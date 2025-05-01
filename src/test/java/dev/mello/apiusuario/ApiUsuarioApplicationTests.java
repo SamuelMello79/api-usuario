da30f94a0b91e7c0dada2774197da4f25e92b810
SamuelMello79 <mello.developer797@gmail.com>
2025-05-01 19:59:49 -0300

    [CHORE] Adicionando projeto inicial

---
+++ src/test/java/dev/mello/apiusuario/ApiUsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.mello.apiusuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
